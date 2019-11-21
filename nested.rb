161b6a92b6c0fcbdfb9f33b4bf8d03d0513edd85
wolfene2001 <wolfene2001@yahoo.com>
2019-11-21 17:28:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,7 +111,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
 
-    #return changing_alan = 
+    #return changing_alan =
 
 		programmer_hash[:alan_kay][:known_for] = "GUI"
 
